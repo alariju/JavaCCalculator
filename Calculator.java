f4f4e4e1495cdf3820c87b4f355381c0e11df26e
alariastec <alarias@ic-itcr.ac.cr>
2014-03-20 23:33:33 -0600

    Update Calculator.java
    Comments added

--- Calculator.java
+++ Calculator.java
@@ -1,12 +1,21 @@
+/*
+	Clase Calculator
+*/
 public class Calculator{
+	
+	//Se carga la librería operations.dll
+	//Esta se genera compilando los archivos Calculator.c y Calculator.h
 	static{
 		System.loadLibrary("operations");
 	}
 
+	//Funciones nativas, que son programadas en C, pero se llaman
+	//mediante esta función
 	public native int ADDNUMBERS(int x, int y);
 	public native int SUBNUMBERS(int x, int y);
 	public native int MULNUMBERS(int x, int y);
 	public native int DIVNUMBERS(int x, int y);
 
+	//Constructor de la clase Calculator
 	public Calculator(){};
 }
